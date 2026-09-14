a65acb0a70ef02b95b686ba70c149e4651b80171
geeta <rwtgeet6@gmail.com>
2026-09-14 13:52:28 +0530

    add index.js

---
+++ index.js
@@ -0,0 +1,104 @@
+// console.log("geeta");
+// let name="geeta";
+// let numType=Number(name);
+// console.log(typeof numType);
+// console.log(numType);
+
+// let val1="";
+// let boolVal=Boolean(val1);
+// console.log(boolVal); 
+// console.log(typeof boolVal); 
+
+let str=new String('geeta');
+console.log(typeof str)
+
+// console.log(str.charAt(4))
+
+// console.log(str.concat(" rawat"))
+
+// console.log(str.includes("ga"))
+
+// console.log(str.indexOf("g"))
+
+// console.log(str.length);
+
+// console.log(str.match("ta"))
+
+// console.log(str.replace("geeta","rawat"));
+
+// console.log(str.replaceAll("e","4"));
+
+// console.log(str.slice(2,4));
+
+// console.log(str.concat(" rawat").split(" "));
+
+// console.log(str.startsWith("G"));
+
+// console.log(str.substr(3,5));
+
+// console.log(str.substring(2,5));
+
+let name="geeta";
+// console.log(name.charAt(4))
+
+// console.log(name.includes("g"))
+
+// console.log(name.concat(" rawat"));
+
+// console.log(name.indexOf("e"))
+
+// console.log(name.length)
+
+// console.log(name.match("gee"))
+
+// console.log(name.replace("e","i"))
+// console.log(name.replaceAll("e","i"));
+
+// console.log(name.slice(2,4));
+
+// console.log(name.split(""))
+
+// console.log(name.startsWith("g"))
+
+// console.log(name.substr(2,4));
+
+// console.log(name.substring(1,3));
+
+// console.log(name.toLowerCase());
+
+// console.log(name.toUpperCase());
+
+// console.log(name.trim());
+
+// let num=1000000;
+// console.log(num.toLocaleString("en-IN"));
+
+// console.log(-10);
+// console.log(Math.abs(-10));
+
+
+let min=10;
+let max=20;
+
+// console.log(Math.round(Math.random()*(max-min))+min);
+
+// let date=new Date();
+// console.log(date);
+// console.log(date.toString());
+// console.log(date.toDateString());
+// console.log(date.toISOString());
+// console.log(date.toJSON());
+// console.log(date.toLocaleDateString());
+// console.log(date.toLocaleString());
+
+
+let date=new Date();
+// console.log(Math.floor(Date.now()/1000))
+
+// console.log(date.getDate());
+// console.log(date.getDay());
+// console.log(date.getFullYear());
+// console.log(date.getMonth());
+// console.log(date.getTime());
+// console.log(date.toDateString());
+// console.log(date.toLocaleDateString());
